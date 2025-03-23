62c3b563e1dc44cd0d5be7d02f8a1daf5700518f
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-03-23 16:08:45 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itson/loginautenticacioncastrog/RegisterActivity.kt
@@ -0,0 +1,82 @@
+package mx.edu.itson.loginautenticacioncastrog
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+        auth = Firebase.auth
+
+        val email: EditText = findViewById(R.id.etrEmail)
+        val password: EditText = findViewById(R.id.etrPassword)
+        val confirmPassword: EditText = findViewById(R.id.etrConfirmPassword)
+        val errorTv: TextView = findViewById(R.id.tvrError)
+        val goLogin: Button = findViewById(R.id.btnGoLogin)
+        val button: Button = findViewById(R.id.btnRegister)
+        errorTv.visibility = View.INVISIBLE
+
+        goLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener({
+            if(email.text.isEmpty() || password.text.isEmpty() || confirmPassword.text.isEmpty()){
+                errorTv.text = "Todos los campos deben de ser llenados"
+                errorTv.visibility = View.VISIBLE
+            } else if(!password.text.toString().equals(confirmPassword.text.toString())){
+                errorTv.text = "Las contraseñas no coinciden"
+                errorTv.visibility = View.VISIBLE
+            }else{
+                errorTv.visibility = View.INVISIBLE
+                signIn(email.text.toString(), password.text.toString())
+            }
+        })
+
+    }
+
+
+    fun signIn(email: String, password: String) {
+        Log.d("INFO","email: ${email}, password: ${password}")
+        auth.createUserWithEmailAndPassword (email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("INFO","signInWithEmail: success")
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.setFlags (Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("ERROR","signInWithEmail: failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "El registro falló.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+
+            }
+    }
+}
